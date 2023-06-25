16d5cca06ea945142e28834933c03ddf9d21bd5f
habyaad <adeomotoshan@gmail.com>
2023-06-25 15:41:04 +0100

    feat: create logger service

---
+++ lib/services/logger_service.dart
@@ -0,0 +1,18 @@
+import 'package:logger/logger.dart';
+
+class LoggerService {
+  var logger = Logger();
+
+  void error(String msg){
+    logger.e(msg, 'Error');
+  }
+  void info(String msg){
+    logger.i(msg, 'Info');
+  }
+  void warning(String msg){
+    logger.w(msg, 'Warning');
+  }
+  void message(String msg){
+    logger.d(msg, 'Message');
+  }
+}
