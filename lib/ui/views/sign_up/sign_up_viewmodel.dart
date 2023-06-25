8d091bcf2b005bbb7f879007c55b5a4ffa35e284
habyaad <adeomotoshan@gmail.com>
2023-06-25 21:45:05 +0100

    feat: implement sign up logic from auth service

--- lib/ui/views/sign_up/sign_up_viewmodel.dart
+++ lib/ui/views/sign_up/sign_up_viewmodel.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/cupertino.dart';
+import 'package:soro_soke/app/app.locator.dart';
+import 'package:soro_soke/models/api_response_model.dart';
+import 'package:soro_soke/services/authentication_service.dart';
+import 'package:soro_soke/services/toast_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class SignUpViewModel extends BaseViewModel {
   final formKey = GlobalKey<FormState>();
   final usernameController = TextEditingController();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  final _authService = locator<AuthenticationService>();
+  final _navigationService = locator<NavigationService>();
+  final _toastService = locator<ToastService>();
+
+  void signUp() async{
+    String name = usernameController.text;
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    ApiResponse response = await _authService.signUp(name, email, password);
+
+    if(response.success){
+      _toastService.success("${response.message}, login to access your account");
+      _navigationService.replaceWith("/login-view");
+    }else{
+      _toastService.error(response.message);
+    }
+  }
 }
