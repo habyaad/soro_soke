46dd369c0a514e61fb752b3819ddbe24fad2bb59
habyaad <adeomotoshan@gmail.com>
2023-06-25 14:14:12 +0100

    feat: create button widget

---
+++ lib/ui/common/general_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:soro_soke/ui/common/app_colors.dart';
+import 'package:soro_soke/ui/common/ui_helpers.dart';
+
+class GeneralButton extends StatelessWidget {
+  final void Function() onPressed;
+  final String buttonText;
+
+  const GeneralButton({super.key, required this.onPressed, required this.buttonText});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: kcPrimaryColor
+        ),
+        width: screenWidth(context) -20 ,
+        height: 46,
+        child: Center(child: Text(buttonText, style: const TextStyle(color: Colors.white, fontSize: 18),)),
+      ),
+    );
+  }
+}
