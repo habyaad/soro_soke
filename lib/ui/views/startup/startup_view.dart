aa4e4575dbb76fbd7e48f824fa79d2e9b687f6ec
habyaad <adeomotoshan@gmail.com>
2023-06-25 14:17:21 +0100

    refactor: change splash screen text

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:stacked/stacked.dart';
 import 'package:soro_soke/ui/common/ui_helpers.dart';
 
+import '../../common/app_colors.dart';
 import 'startup_viewmodel.dart';
 
 class StartupView extends StackedView<StartupViewModel> {
@@ -16,29 +17,28 @@ class StartupView extends StackedView<StartupViewModel> {
   ) {
     return const Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'STACKED',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Loading ...', style: TextStyle(fontSize: 16)),
-                horizontalSpaceSmall,
-                SizedBox(
-                  width: 16,
-                  height: 16,
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                    strokeWidth: 6,
-                  ),
-                )
-              ],
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Soro Soke',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+              ),
+              Text(
+                  'Talk to your loved ones on what is lingering on your mind',
+                  style: TextStyle(fontSize: 16)),
+              verticalSpaceMedium,
+              SizedBox(
+                width: 16,
+                height: 16,
+                child: CircularProgressIndicator(
+                  color: kcPrimaryColor,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
