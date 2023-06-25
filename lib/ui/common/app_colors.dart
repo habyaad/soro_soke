d45c0f7710739694eb033772fae9395252025fb3
habyaad <adeomotoshan@gmail.com>
2023-06-25 14:14:32 +0100

    style: change color scheme

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color kcPrimaryColor = Color(0xFF9600FF);
-const Color kcPrimaryColorDark = Color(0xFF300151);
+const Color kcPrimaryColor = Color(0xFFEA6A2E);
+const Color kcPrimaryColorDark = Color(0xFFA82800);
 const Color kcDarkGreyColor = Color(0xFF1A1B1E);
 const Color kcMediumGrey = Color(0xFF474A54);
 const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
