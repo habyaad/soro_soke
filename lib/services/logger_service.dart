03f4b7a6a1cd5393227eb09281039769436333b7
habyaad <adeomotoshan@gmail.com>
2023-06-29 07:39:36 +0100

    refactor: refactor lines

--- lib/services/logger_service.dart
+++ lib/services/logger_service.dart
@@ -3,16 +3,19 @@ import 'package:logger/logger.dart';
 class LoggerService {
   var logger = Logger();
 
-  void error(String msg){
+  void error(String msg) {
     logger.e(msg, 'Error');
   }
-  void info(String msg){
+
+  void info(String msg) {
     logger.i(msg, 'Info');
   }
-  void warning(String msg){
+
+  void warning(String msg) {
     logger.w(msg, 'Warning');
   }
-  void message(String msg){
+
+  void message(String msg) {
     logger.d(msg, 'Message');
   }
 }
