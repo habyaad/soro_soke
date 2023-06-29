7df2443c7eee8dc8330dfa983188b79a4f9561de
habyaad <adeomotoshan@gmail.com>
2023-06-29 12:59:08 +0100

    refactor: increase toast length

--- lib/services/toast_service.dart
+++ lib/services/toast_service.dart
@@ -5,7 +5,7 @@ class ToastService {
   static void show(String msg, Color color) {
     Fluttertoast.showToast(
         msg: msg,
-        toastLength: Toast.LENGTH_SHORT,
+        toastLength: Toast.LENGTH_LONG,
         gravity: ToastGravity.BOTTOM,
         timeInSecForIosWeb: 2,
         backgroundColor: color,
