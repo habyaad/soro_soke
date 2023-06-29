1822ae2ea7cb3e705961b8efe8296481295ad70b
habyaad <adeomotoshan@gmail.com>
2023-06-29 13:21:11 +0100

    refactor: change the startup logic

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:soro_soke/services/user_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:soro_soke/app/app.locator.dart';
 import 'package:soro_soke/app/app.router.dart';
@@ -5,14 +7,24 @@ import 'package:stacked_services/stacked_services.dart';
 
 class StartupViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  final _userService = locator<UserService>();
 
+  Stream<User?> get authStream => _userService.authStream;
   // Place anything here that needs to happen before we get into the application
-  Future runStartupLogic() async {
+ /* Future runStartupLogic() async {
     await Future.delayed(const Duration(seconds: 3));
 
     // This is where you can make decisions on where your app should navigate when
     // you have custom startup logic
+  }*/
 
-    _navigationService.replaceWithSignUpView();
+  void navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 3));
+    _navigationService.replaceWith("/home-view");
+  }
+
+  void navigateToLogin() async {
+    await Future.delayed(const Duration(seconds: 3));
+    _navigationService.replaceWithLoginView();
   }
 }
