117b1a2468d38dd7015f13868dcbe5bf89ec2e92
habyaad <adeomotoshan@gmail.com>
2023-07-01 11:09:34 +0100

    feat: add extra field and methods to user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,21 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserModel {
-  final String id;
-  final String name;
+  final String firstName;
+  final String lastName;
   final String email;
   final String password;
+  final String uid;
+  final DateTime dateCreated;
 
   UserModel({
-    required this.id,
-    required this.name,
+    required this.firstName,
+    required this.lastName,
     required this.email,
     required this.password,
+    required this.uid,
+    required this.dateCreated,
   });
+
+  DateTime get currentDateTime => DateTime.now();
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
-        id: json['id'],
-        name: json['name'],
+        firstName: json['firstName'],
+        lastName: json['lastName'],
         email: json['email'],
-        password: json['password']);
+        password: json['password'],
+        uid: json['uid'],
+        dateCreated: json['dateCreated']
+    );
+  }
+
+  Map<String, dynamic> toJson(UserModel user) {
+    return {
+      'firstName': user.firstName,
+      'lastName': user.lastName,
+      'email': user.email,
+      'password': user.password,
+      'uid': user.uid,
+      'dateCreated': FieldValue.serverTimestamp()
+    };
   }
 }
