6a969b9e08c3f2ec627c2b57e023cc84b78b504a
habyaad <adeomotoshan@gmail.com>
2023-07-01 11:10:41 +0100

    fix: remove scaffold

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:soro_soke/ui/common/general_button.dart';
@@ -16,32 +17,26 @@ class HomeView extends StackedView<HomeViewModel> {
   ) {
     viewModel.initializeUser();
 
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      'Hello, ${viewModel.currentUser!.displayName}!',
-                      style: const TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
-                      ),
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'Hello, ${viewModel.currentUser!.displayName}!',
+                    style: const TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w900,
                     ),
-                    verticalSpaceMassive,
-                    GeneralButton(onPressed: (){
-                      viewModel.logOut();
-                    }, buttonText: "Sign Out")
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
@@ -54,7 +49,7 @@ class HomeView extends StackedView<HomeViewModel> {
   ) =>
       HomeViewModel();
 
-  @override
+/*@override
   void onViewModelReady(HomeViewModel viewModel) => SchedulerBinding.instance
-      .addPostFrameCallback((timeStamp) => viewModel.initializeUser());
+      .addPostFrameCallback((timeStamp) => viewModel.initializeUser());*/
 }
