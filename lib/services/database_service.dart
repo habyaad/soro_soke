520965923a50ba0a63f3ec641cd3ff5368266824
habyaad <adeomotoshan@gmail.com>
2023-07-23 12:20:12 +0100

    chore: update android version

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,1 +1,24 @@
-class DatabaseService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../app/app.locator.dart';
+import '../models/user_model.dart';
+import 'logger_service.dart';
+
+class DatabaseService {
+  final _loggerService = locator<LoggerService>();
+
+  final FirebaseFirestore store = FirebaseFirestore.instance;
+
+  Future<void> addUser(UserModel user) {
+    final CollectionReference usersCollection = store.collection('users');
+
+    Map<String, dynamic> data = user.toJson();
+
+    return usersCollection
+        .doc(user.uid)
+        .set(data)
+        .then((value) => _loggerService.info("User Added"))
+        .catchError(
+            (error) => _loggerService.error("Failed to add user: $error"));
+  }
+}
