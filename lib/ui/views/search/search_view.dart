a40d40296336b5d63a3307b516b5c96e20f9cc29
habyaad <adeomotoshan@gmail.com>
2023-07-23 12:23:15 +0100

    feat: implement search UI and method

--- lib/ui/views/search/search_view.dart
+++ lib/ui/views/search/search_view.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:soro_soke/utils/app_colors.dart';
+import 'package:soro_soke/utils/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
 
 import 'search_viewmodel.dart';
@@ -12,10 +16,65 @@ class SearchView extends StackedView<SearchViewModel> {
     SearchViewModel viewModel,
     Widget? child,
   ) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpace(40),
+
+              const Text(
+                "Search for users",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24, color: kcPrimaryColor),
+              ),
+              const Text(
+                "Find loved ones to send a message to",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(hintText: "username"),
+                controller: viewModel.searchController,
+                onChanged: (val) {
+                  viewModel.updateSearchQuery(val);
+                },
+              ),
+              verticalSpaceMedium,
+              Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: viewModel.getResults(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else {
+                      final users = snapshot.data!.docs;
+
+                      return ListView.builder(
+                        itemCount: users.length,
+                        itemBuilder: (context, index) {
+                          final user = users[index].data() as Map;
+                          if (kDebugMode) {
+                            print(user);
+                          }
+                          return ListTile(
+                            title: Text(user['name']),
+                            subtitle: Text(user['email']),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/ui/views/search/search_viewmodel.dart
+++ lib/ui/views/search/search_viewmodel.dart
@@ -1,3 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:stacked/stacked.dart';
+import '../../../app/app.locator.dart';
+import '../../../services/database_service.dart';
 
-class SearchViewModel extends BaseViewModel {}
+class SearchViewModel extends BaseViewModel {
+  final _databaseService = locator<DatabaseService>();
+  final TextEditingController searchController = TextEditingController();
+
+  String _searchQuery = "";
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> getResults() {
+    return _databaseService.store
+        .collection('users')
+        .where('name', isGreaterThanOrEqualTo: _searchQuery.toUpperCase())
+        .snapshots();
+  }
+
+  void updateSearchQuery(String query) {
+    _searchQuery = query;
+    rebuildUi();
+  }
+}
