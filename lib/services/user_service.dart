540eac7db16df91cce91630c62ca6db3312321fc
habyaad <adeomotoshan@gmail.com>
2023-07-23 12:22:25 +0100

    feat: write method to create new user in database

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,11 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:soro_soke/models/user_model.dart';
+import 'package:soro_soke/services/database_service.dart';
 import '../app/app.locator.dart';
 import 'authentication_service.dart';
 
 class UserService {
   final _authService = locator<AuthenticationService>();
+  final _databaseService = locator<DatabaseService>();
 
   User? get currentUser => _authService.auth.currentUser;
 
   Stream<User?> get authStream => _authService.auth.authStateChanges();
+
+  Future<void> createUser(UserModel user) async{
+    await _databaseService.addUser(user);
+  }
 }
