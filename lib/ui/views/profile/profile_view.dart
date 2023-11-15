b74ac32678b87ad04d7a4163361d57d46d5375b7
habyaad <adeomotoshan@gmail.com>
2023-11-15 08:05:02 +0100

    refactor: refactor profile view and extract profile detail tile widget

--- lib/ui/views/profile/profile_view.dart
+++ lib/ui/views/profile/profile_view.dart
@@ -3,6 +3,7 @@ import 'package:soro_soke/utils/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
 
 import '../../common/general_button.dart';
+import '../../common/profile_detail_tile.dart';
 import 'profile_viewmodel.dart';
 
 class ProfileView extends StackedView<ProfileViewModel> {
@@ -24,14 +25,14 @@ class ProfileView extends StackedView<ProfileViewModel> {
               children: [
                 const SizedBox(height: 20),
                 GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     print('clicked');
                     viewModel.uploadPhoto();
                   },
                   child: CircleAvatar(
                     radius: 60,
-                    backgroundImage:
-                        NetworkImage(viewModel.currentUser!.photoURL.toString()),
+                    backgroundImage: NetworkImage(
+                        viewModel.currentUser!.photoURL.toString()),
                   ),
                 ),
                 const SizedBox(height: 20),
@@ -83,48 +84,4 @@ class ProfileView extends StackedView<ProfileViewModel> {
     BuildContext context,
   ) =>
       ProfileViewModel();
-}
-
-class ProfileDetailTile extends StatelessWidget {
-  final IconData icon;
-  final String label;
-  final String value;
-
-  const ProfileDetailTile({
-    Key? key,
-    required this.icon,
-    required this.label,
-    required this.value,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Row(
-        children: [
-          Icon(
-            icon,
-            size: 24,
-            color: Colors.blue,
-          ),
-          const SizedBox(width: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                label,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 5),
-              Text(value),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
 }
