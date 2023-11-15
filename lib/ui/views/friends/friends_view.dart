41a0fb82d17c58cca6d579446c614ba7b24b2c78
habyaad <adeomotoshan@gmail.com>
2023-11-15 08:03:36 +0100

    feat: implement friends screen

--- lib/ui/views/friends/friends_view.dart
+++ lib/ui/views/friends/friends_view.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
+import 'package:soro_soke/utils/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
 
+import '../../../models/user_model.dart';
 import 'friends_viewmodel.dart';
 
 class FriendsView extends StackedView<FriendsViewModel> {
@@ -13,9 +17,133 @@ class FriendsView extends StackedView<FriendsViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      appBar: AppBar(
+        title: const Text('My Friends'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpaceSmall,
+              FutureBuilder<int>(
+                  future: viewModel.requests(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Shimmer(
+                        duration: const Duration(seconds: 4),
+                        //Default value
+                        interval: const Duration(seconds: 1),
+                        //Default value: Duration(seconds: 0)
+                        color: Colors.white,
+                        //Default value
+                        colorOpacity: 0.8,
+                        //Default value
+                        enabled: true,
+                        //Default value
+                        direction: const ShimmerDirection.fromLTRB(),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey.withOpacity(.1),
+                              borderRadius: BorderRadius.circular(8)),
+                          width: double.infinity,
+                          height: 40,
+                        ),
+                      );
+                    } else if (snapshot.hasError) {
+                      if (kDebugMode) {
+                        print(snapshot.error);
+                      }
+                      return
+                          //Text('Error: ${snapshot.error}');
+                          const Center(child: Text('Error fetching data'));
+                    } else {
+                      return GestureDetector(
+                        onTap: () {
+                          viewModel.goToFriendRequest();
+                        },
+                        child: Text(
+                          'Friend requests(${snapshot.data!})',
+                          style: const TextStyle(fontSize: 24),
+                        ),
+                      );
+                    }
+                  }),
+              verticalSpaceMedium,
+              StreamBuilder(
+                stream: viewModel.getFriends(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(child: Text("No friends"));
+                  } else {
+                    List<Future<UserModel>> friends = snapshot.data!;
+
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: friends.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return FutureBuilder(
+                          future: friends[index],
+                          /*shrinkWrap: true,
+                          itemCount: friends.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text(friends[index]),
+                              subtitle:
+                                  Text('Became Friends on: ${friends[index]}'),
+                            );
+                          },*/
+                          builder: (BuildContext context,
+                              AsyncSnapshot<dynamic> userSnapshot) {
+                            if (userSnapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Shimmer(
+                                duration: const Duration(seconds: 4),
+                                //Default value
+                                interval: const Duration(seconds: 1),
+                                //Default value: Duration(seconds: 0)
+                                color: Colors.white,
+                                //Default value
+                                colorOpacity: 0.8,
+                                //Default value
+                                enabled: true,
+                                //Default value
+                                direction: const ShimmerDirection.fromLTRB(),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey.withOpacity(0.1),
+                                      borderRadius: BorderRadius.circular(8)),
+                                  width: double.infinity,
+                                  height: 40,
+                                ),
+                              ); // Show a loader while waiting for the future to resolve
+                            } else if (userSnapshot.hasError) {
+                              return Text(
+                                  'Error fetching user: ${userSnapshot.error}');
+                            } else if (!userSnapshot.hasData) {
+                              return const Text('No user data available');
+                            }
+
+                            // Once the future resolves, display the user data
+                            return ListTile(
+                              onTap: () {
+                                viewModel.goToUserProfile(userSnapshot.data!);
+                              },
+                              title: Text(userSnapshot.data!.name),
+                              subtitle: Text(userSnapshot.data!.email),
+                            );
+                            // Include more UI elements to display user details
+                          },
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/ui/views/friends/friends_viewmodel.dart
+++ lib/ui/views/friends/friends_viewmodel.dart
@@ -1,3 +1,44 @@
+import 'package:soro_soke/app/app.router.dart';
+import 'package:soro_soke/models/user_model.dart';
+import 'package:soro_soke/services/user_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class FriendsViewModel extends BaseViewModel {}
+import '../../../app/app.locator.dart';
+import '../../../services/friend_service.dart';
+
+class FriendsViewModel extends BaseViewModel {
+  final _friendService = locator<FriendService>();
+  final _userService = locator<UserService>();
+  final _navigationService = locator<NavigationService>();
+
+  List<UserModel> friends = [];
+
+  Stream<List<Future<UserModel>>>? getFriends() {
+    return _friendService.getFriends()?.map((ids) {
+      return ids.map((id) {
+        return _userService.getUserFromID(id).then((snapshot) {
+          print(snapshot!.email);
+          return snapshot;
+        });
+      }).toList();
+    });
+  }
+
+  Future<UserModel?> getUser(uid) {
+    return _userService.getUserFromID(uid);
+  }
+
+  Future<int>? requests() async {
+    final result = await _friendService.getFriendRequests();
+    return result!.size;
+  }
+
+  void goToFriendRequest() {
+    _navigationService.navigateToFriendRequestsView();
+  }
+
+  void goToUserProfile(user) {
+    _navigationService.navigateToUserProfileView(user: user);
+  }
+}
