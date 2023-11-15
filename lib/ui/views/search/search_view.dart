1e1822e1daac10ad464962507c06628af156e719
habyaad <adeomotoshan@gmail.com>
2023-11-15 08:02:12 +0100

    feat: re-implement the search feature

--- lib/ui/views/search/search_view.dart
+++ lib/ui/views/search/search_view.dart
@@ -1,14 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:soro_soke/ui/common/general_button.dart';
 import 'package:soro_soke/utils/app_colors.dart';
 import 'package:soro_soke/utils/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
 
+import '../../../models/user_model.dart';
 import 'search_viewmodel.dart';
 
 class SearchView extends StackedView<SearchViewModel> {
   const SearchView({Key? key}) : super(key: key);
+
+  @override
+  void onViewModelReady(SearchViewModel viewModel) {
+    super.onViewModelReady(viewModel);
+    viewModel.initializeUser();
+  }
+
+  @override
+  void onDispose(SearchViewModel viewModel) {
+    // TODO: implement onDispose
+    super.onDispose(viewModel);
+  }
 
   @override
   Widget builder(
@@ -16,10 +30,6 @@ class SearchView extends StackedView<SearchViewModel> {
     SearchViewModel viewModel,
     Widget? child,
   ) {
-    print('searchQuery');
-
-    print(viewModel.searchQuery);
-
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
@@ -42,42 +52,47 @@ class SearchView extends StackedView<SearchViewModel> {
                 ),
               ),
               TextFormField(
-                  decoration: const InputDecoration(hintText: "username"),
-                  //controller: viewModel.searchController,
-                  onChanged: viewModel.updateSearchQuery),
-              verticalSpaceMedium,
-              Expanded(
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: viewModel.getResults(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    } else {
-                      final users = snapshot.data!.docs;
-                      return ListView.builder(
-                        itemCount: users.length,
-                        itemBuilder: (context, index) {
-                          final user = users[index].data() as Map;
-                          if (kDebugMode) {
-                            print(user);
-                          }
-                          return ListTile(
-                            /* leading: CircleAvatar(
-                            radius: 30,
-                            backgroundImage:
-                            NetworkImage(user['photoURL']),
-                          ),*/
-                            title: Text(user['name']),
-                            subtitle: Text(user['email']),
-                          );
-                        },
-                      );
+                decoration: const InputDecoration(hintText: "username"),
+                controller: viewModel.searchController,
+              ),
+              verticalSpaceLarge,
+              GeneralButton(
+                  onPressed: () {
+                    viewModel.getResults();
+                  },
+                  buttonText: "Search"),
+              verticalSpaceLarge,
+              Visibility(
+                visible: viewModel.results.isNotEmpty,
+                replacement: verticalSpaceTiny,
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  itemCount: viewModel.results.length,
+                  itemBuilder: (context, index) {
+                    final UserModel user = viewModel.results[index];
+                    if (kDebugMode) {
+                      print(user);
                     }
+                    return ListTile(
+                      onTap: () {
+                        if (user.uid == viewModel.currentUser!.uid) {
+                          viewModel.goToProfile();
+                        }
+                        viewModel.goToUserProfile(user);
+                      },
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(user.profilePhotoUrl!),
+                      ),
+                      title: Text(user.name),
+                      subtitle: Text(user.email),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return verticalSpace(24);
                   },
                 ),
-              ),
+              )
             ],
           ),
         ),
--- lib/ui/views/search/search_viewmodel.dart
+++ lib/ui/views/search/search_viewmodel.dart
@@ -1,26 +1,61 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:soro_soke/app/app.router.dart';
+import 'package:soro_soke/models/user_model.dart';
+import 'package:soro_soke/services/user_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 import '../../../app/app.locator.dart';
 import '../../../services/database_service.dart';
 
 class SearchViewModel extends BaseViewModel {
-  final _databaseService = locator<DatabaseService>();
+  final NavigationService _navigationService = locator<NavigationService>();
+  final UserService _userService = locator<UserService>();
+  final DatabaseService _databaseService = locator<DatabaseService>();
   final TextEditingController searchController = TextEditingController();
 
-  String searchQuery = "";
+  User? currentUser;
 
-  Stream<QuerySnapshot<Map<String, dynamic>>> getResults() {
-    return _databaseService.store
-        .collection('users')
-        .where('name', isGreaterThanOrEqualTo: searchQuery.toUpperCase())
-        .snapshots();
+  void initializeUser() async {
+    currentUser = _userService.currentUser;
   }
 
-  void updateSearchQuery(String query) {
-    print(query);
-    searchQuery = query;
-    print('about to rebuild');
+  String searchQuery = "";
+  List<UserModel> results = [];
+
+  Future<void> getResults() async {
+    results = [];
+    print(searchController.text);
+    QuerySnapshot querySnapshot = await _databaseService.store
+        .collection('users')
+        .where('name', isEqualTo: searchController.text)
+        .get();
+    if (querySnapshot.docs.isNotEmpty) {
+      // User found
+      for (var doc in querySnapshot.docs) {
+        print('User found: ${doc.get("name")}');
+        UserModel user = UserModel(
+            name: doc.get("name"),
+            email: doc.get("email"),
+            uid: doc.get("uid"),
+            profilePhotoUrl: doc.get("profilePhotoUrl")
+            // Use the actual field name here
+            );
+        results.add(user);
+        // Access user details using doc.data()
+      }
+    } else {
+      print('User not found');
+    }
     rebuildUi();
+  }
+
+  void goToUserProfile(UserModel user) {
+    _navigationService.navigateToUserProfileView(user: user);
+  }
+
+  void goToProfile() {
+    _navigationService.navigateToProfileView();
   }
 }
