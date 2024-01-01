0854f7d2c4777f2f34cd1bafebde09ab30eeae8a
habyaad <adeomotoshan@gmail.com>
2024-01-01 19:46:16 +0100

    refactor: remove unused code

--- lib/ui/views/friend_requests/friend_requests_viewmodel.dart
+++ lib/ui/views/friend_requests/friend_requests_viewmodel.dart
@@ -6,23 +6,16 @@ import 'package:stacked_services/stacked_services.dart';
 
 import '../../../app/app.locator.dart';
 import '../../../models/chat_model.dart';
-import '../../../models/user_model.dart';
 import '../../../services/friend_service.dart';
 import '../../../services/user_service.dart';
 
 class FriendRequestsViewModel extends BaseViewModel {
   final _friendService = locator<FriendService>();
-  final _userService = locator<UserService>();
   final _navigationService = locator<NavigationService>();
   final _toastService = locator<ToastService>();
 
   Stream<QuerySnapshot<Map<String, dynamic>>>? requests() {
     return _friendService.getFriendRequests();
-    /*for (int i = 0; i < result!.docs.length; i++) {
-      QueryDocumentSnapshot<Object?> here = result.docs[i];
-      UserModel? user = await _userService.getUserFromID(here.id);
-      requests.add(user!);
-    }*/
 
   }
 
