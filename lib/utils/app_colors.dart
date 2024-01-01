60319ae6ea4f890256cb8fc3f1186e13192a8a67
habyaad <adeomotoshan@gmail.com>
2024-01-01 19:45:23 +0100

    feat: create color palette for app

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -7,3 +7,12 @@ const Color kcMediumGrey = Color(0xFF474A54);
 const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
 const Color kcVeryLightGrey = Color(0xFFE3E3E3);
 const Color kcBackgroundColor = kcDarkGreyColor;
+
+class AppColors {
+  AppColors._();
+  static const Color primaryColor = Color(0xFF2B0254);
+  static const Color backgroundColor = Color(0xFF261C2C);
+  static const Color lightPrimaryColor = Color(0xFF941BA6);
+  static const Color darkPrimaryColor = Color(0xFF4E0E57);
+
+}
