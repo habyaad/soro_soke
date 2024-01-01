79225c93837a1259c5045e655388e4d88774a87b
habyaad <adeomotoshan@gmail.com>
2024-01-01 19:38:16 +0100

    feat: create method to get conversation id between friends

--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -1,13 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:soro_soke/services/user_service.dart';
+
 import '../app/app.locator.dart';
 import '../models/message_model.dart';
 import 'database_service.dart';
 
 class MessageService {
   final _databaseService = locator<DatabaseService>();
+  final _userService = locator<UserService>();
+
+  String getConversationID(String friendUid) {
+    print("mine: ${_userService.currentUser!.uid.hashCode}");
+    print("yours: ${friendUid.hashCode}");
+    print("--------------------------------------");
+
+    if (_userService.currentUser!.uid.hashCode < friendUid.hashCode) {
+      print("${_userService.currentUser!.uid}$friendUid");
+      return "${_userService.currentUser!.uid}$friendUid";
+    } else {
+      print("$friendUid${_userService.currentUser!.uid}");
+      return "$friendUid${_userService.currentUser!.uid}";
+    }
+  }
 
 // Save a message
   Future<void> saveMessage(Message message) async {
-    await _databaseService.store.collection('messages').add({
+    await _databaseService.store
+        .collection('messages/${getConversationID(message.receiverId)}/chat')
+        .add({
       'senderId': message.senderId,
       'receiverId': message.receiverId,
       'content': message.content,
@@ -16,24 +36,18 @@ class MessageService {
   }
 
 // Retrieve messages between two users
-  Stream<List<Message>> getMessagesBetweenUsers(
-      String user1Id, String user2Id) {
-    return _databaseService.store
-        .collection('messages')
-        .where('senderId', whereIn: [user1Id, user2Id])
-        .where('receiverId', whereIn: [user1Id, user2Id])
-        .orderBy('timestamp', descending: true)
-        .snapshots()
-        .map((snapshot) {
-          return snapshot.docs.map((doc) {
-            Map<String, dynamic> data = doc.data();
-            return Message(
-              senderId: data['senderId'],
-              receiverId: data['receiverId'],
-              content: data['content'],
-              timestamp: data['timestamp'].toDate(),
-            );
-          }).toList();
-        });
+
+  Stream<QuerySnapshot<Object?>> getMessagesBetweenUsers(String friendUid) {
+    try {
+      return _databaseService.store
+          .collection('messages/${getConversationID(friendUid)}/chat')
+          //.where("senderId", isEqualTo: user1Id)
+          .orderBy("timestamp", descending: true)
+          .snapshots();
+    } catch (e) {
+      print('Error retrieving messages: $e');
+      // Handle errors more gracefully in a production app
+      throw e;
+    }
   }
 }
