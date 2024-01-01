c8b0c05d2016988e3f87fa695e7bdb711cdc8ce6
habyaad <adeomotoshan@gmail.com>
2024-01-01 19:35:46 +0100

    refactor: modify user profile screen

--- lib/ui/views/user_profile/user_profile_view.dart
+++ lib/ui/views/user_profile/user_profile_view.dart
@@ -5,13 +5,15 @@ import 'package:soro_soke/models/user_model.dart';
 import 'package:soro_soke/utils/enums.dart';
 import 'package:stacked/stacked.dart';
 
+import '../../../models/chat_model.dart';
+import '../../../utils/app_colors.dart';
 import '../../../utils/ui_helpers.dart';
 import '../../common/general_button.dart';
 import '../../common/profile_detail_tile.dart';
 import 'user_profile_viewmodel.dart';
 
 class UserProfileView extends StackedView<UserProfileViewModel> {
-  final UserModel user;
+  final ChatModel user;
 
   const UserProfileView(this.user, {Key? key}) : super(key: key);
 
@@ -23,50 +25,41 @@ class UserProfileView extends StackedView<UserProfileViewModel> {
   ) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: AppColors.backgroundColor,
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white),
+          automaticallyImplyLeading: true,
+          backgroundColor: AppColors.backgroundColor,
+        ),
+        floatingActionButton: FutureBuilder<FriendshipState>(
+            future: viewModel.checkState,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SizedBox();
+              } else if (snapshot.hasError) {
+                if (kDebugMode) {
+                  print(snapshot.error);
+                }
+                return const SizedBox();
+              } else {
+                if (snapshot.data == FriendshipState.friends) {
+                  return FloatingActionButton(
+                    onPressed: () {
+                      viewModel.goToChat(user);
+                    },
+                    child: const Icon(Icons.chat),
+                  );
+                }
+                return const SizedBox();
+              }
+            }),
         body: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Center(
             child: Column(
               children: [
-                const SizedBox(height: 20),
-                CircleAvatar(
-                  radius: 60,
-                  backgroundImage:
-                      NetworkImage(user.profilePhotoUrl.toString()),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  user.name,
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  user.email,
-                  style: const TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-                const SizedBox(height: 20),
-                const ProfileDetailTile(
-                  icon: Icons.phone,
-                  label: 'Phone',
-                  value: '+1 123-456-7890',
-                ),
-                const ProfileDetailTile(
-                  icon: Icons.location_on,
-                  label: 'Location',
-                  value: 'New York, USA',
-                ),
-                const ProfileDetailTile(
-                  icon: Icons.work,
-                  label: 'Occupation',
-                  value: 'Software Engineer',
-                ),
-                // Add more profile detail tiles as needed
-                verticalSpaceMedium,
-                FutureBuilder<FriendshipState>(
-                    future: viewModel.checkState,
+                FutureBuilder<UserModel?>(
+                    future: viewModel.appUser,
                     builder: (context, snapshot) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
                         return Shimmer(
@@ -83,12 +76,71 @@ class UserProfileView extends StackedView<UserProfileViewModel> {
                           direction: const ShimmerDirection.fromLTRB(),
                           child: Container(
                             decoration: BoxDecoration(
-                                color: Colors.grey,
+                                color: Colors.grey.withOpacity(.1),
                                 borderRadius: BorderRadius.circular(8)),
                             width: double.infinity,
-                            height: 40,
+                            height: 100,
                           ),
                         );
+                      } else if (snapshot.hasError) {
+                        if (kDebugMode) {
+                          print(snapshot.error);
+                        }
+                        return
+                            //Text('Error: ${snapshot.error}');
+                            const Center(child: Text('Error fetching data'));
+                      } else {
+                        UserModel user = snapshot.data!;
+                        return Column(
+                          children: [
+                            const SizedBox(height: 20),
+                            CircleAvatar(
+                              radius: 60,
+                              backgroundImage:
+                                  NetworkImage(user.profilePhotoUrl.toString()),
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              user.name,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              user.email,
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.white60),
+                            ),
+                            const SizedBox(height: 20),
+                            const ProfileDetailTile(
+                              icon: Icons.phone,
+                              label: 'Phone',
+                              value: '+1 123-456-7890',
+                            ),
+                            const ProfileDetailTile(
+                              icon: Icons.location_on,
+                              label: 'Location',
+                              value: 'New York, USA',
+                            ),
+                            const ProfileDetailTile(
+                              icon: Icons.work,
+                              label: 'Occupation',
+                              value: 'Software Engineer',
+                            ),
+                          ],
+                        );
+                      }
+                    }),
+                // Add more profile detail tiles as needed
+                verticalSpaceMedium,
+                FutureBuilder<FriendshipState>(
+                    future: viewModel.checkState,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const SizedBox();
                       } else if (snapshot.hasError) {
                         if (kDebugMode) {
                           print(snapshot.error);
@@ -100,8 +152,9 @@ class UserProfileView extends StackedView<UserProfileViewModel> {
                         if (snapshot.data == FriendshipState.pendingTO) {
                           return GeneralButton(
                               onPressed: () {
-                                viewModel.cancelRequest(user.uid, user);
+                                viewModel.cancelRequest(user.uid);
                               },
+                              buttonColor: AppColors.lightPrimaryColor,
                               buttonText: "Cancel Request");
                         } else if (snapshot.data ==
                             FriendshipState.pendingFrom) {
@@ -111,7 +164,7 @@ class UserProfileView extends StackedView<UserProfileViewModel> {
                                 child: GeneralButton(
                                     buttonColor: Colors.green,
                                     onPressed: () {
-                                      viewModel.acceptRequest(user.uid, user);
+                                      viewModel.acceptRequest(user.uid);
                                     },
                                     buttonText: "Accept Request"),
                               ),
@@ -122,7 +175,7 @@ class UserProfileView extends StackedView<UserProfileViewModel> {
                                 child: GeneralButton(
                                     buttonColor: Colors.redAccent,
                                     onPressed: () {
-                                      viewModel.rejectRequest(user.uid, user);
+                                      viewModel.rejectRequest(user.uid);
                                     },
                                     buttonText: "Reject Request"),
                               ),
@@ -131,14 +184,16 @@ class UserProfileView extends StackedView<UserProfileViewModel> {
                         } else if (snapshot.data == FriendshipState.friends) {
                           return GeneralButton(
                               onPressed: () {
-                                viewModel.removeFriend(user.uid, user);
+                                viewModel.removeFriend(user.uid);
                               },
+                              buttonColor: AppColors.lightPrimaryColor,
                               buttonText: "Remove Friend");
                         }
                         return GeneralButton(
                             onPressed: () {
-                              viewModel.sendRequest(user.uid, user);
+                              viewModel.sendRequest(user.uid);
                             },
+                            buttonColor: AppColors.lightPrimaryColor,
                             buttonText: "Send Request");
                       }
                     }),
@@ -155,6 +210,7 @@ class UserProfileView extends StackedView<UserProfileViewModel> {
     // TODO: implement onViewModelReady
     super.onViewModelReady(viewModel);
     viewModel.checkState = viewModel.checkFriendshipState(user.uid);
+    viewModel.appUser = viewModel.getUser(user.uid);
   }
 
   @override
--- lib/ui/views/user_profile/user_profile_viewmodel.dart
+++ lib/ui/views/user_profile/user_profile_viewmodel.dart
@@ -1,19 +1,27 @@
+import 'package:soro_soke/app/app.router.dart';
+import 'package:soro_soke/models/user_model.dart';
 import 'package:soro_soke/services/friend_service.dart';
 import 'package:soro_soke/services/toast_service.dart';
+import 'package:soro_soke/services/user_service.dart';
 import 'package:soro_soke/utils/enums.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 import '../../../app/app.locator.dart';
+import '../../../models/chat_model.dart';
 
 class UserProfileViewModel extends BaseViewModel {
   final _friendService = locator<FriendService>();
+  final _navigationService = locator<NavigationService>();
+  final _userService = locator<UserService>();
   final _toastService = locator<ToastService>();
   Future<FriendshipState>? checkState;
+  Future<UserModel?>? appUser;
 
-  Future<void> sendRequest(friendUid, user) async {
+  Future<void> sendRequest(friendUid) async {
     await _friendService.sendFriendRequest(friendUid);
     _toastService.success("request sent successfully");
-    checkState = checkFriendshipState(user.uid);
+    checkState = checkFriendshipState(friendUid);
     rebuildUi();
   }
 
@@ -33,48 +41,55 @@ class UserProfileViewModel extends BaseViewModel {
     return await _friendService.isFriend(friendUid);
   }
 
-  Future<void> cancelRequest(friendUid, user) async {
+  Future<void> cancelRequest(friendUid) async {
     bool result = await _friendService.cancelRequest(friendUid);
     if (result == true) {
       _toastService.success("request cancelled");
     } else {
       _toastService.success("an error has occurred!");
     }
-    checkState = checkFriendshipState(user.uid);
+    checkState = checkFriendshipState(friendUid);
     rebuildUi();
   }
 
-  Future<void> removeFriend(friendUid, user) async {
+  Future<void> removeFriend(friendUid) async {
     final result = await _friendService.removeFriend(friendUid);
     if (result != null) {
       _toastService.success("friend removed");
     } else {
       _toastService.success("an error has occurred!");
     }
-    checkState = checkFriendshipState(user.uid);
+    checkState = checkFriendshipState(friendUid);
     rebuildUi();
   }
 
-
-  Future<void> acceptRequest(friendID, user) async {
+  Future<void> acceptRequest(friendID) async {
     try {
       await _friendService.acceptRequest(friendID);
       _toastService.success("request accepted");
     } catch (e) {
       _toastService.success("an error occured");
     }
-    checkState = checkFriendshipState(user.uid);
+    checkState = checkFriendshipState(friendID);
     rebuildUi();
   }
 
-  Future<void> rejectRequest(friendID, user) async {
+  Future<void> rejectRequest(friendID) async {
     try {
       await _friendService.rejectRequest(friendID);
       _toastService.success("request rejected");
     } catch (e) {
       _toastService.success("an error occured");
     }
-    checkState = checkFriendshipState(user.uid);
+    checkState = checkFriendshipState(friendID);
     rebuildUi();
+  }
+
+  void goToChat(ChatModel user) {
+    _navigationService.replaceWithChatView(friend: user);
+  }
+
+  Future<UserModel?> getUser(uid){
+    return _userService.getUserFromID(uid);
   }
 }
