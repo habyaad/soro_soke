4151bafb32d959e4cd583623cf39ef3e61454176
habyaad <adeomotoshan@gmail.com>
2024-01-01 19:36:14 +0100

    feat: create light and dark theme for app

---
+++ lib/utils/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final ThemeData lightTheme = ThemeData.light(
+    /*primarySwatch: Colors.blue,
+    textTheme: GoogleFonts.mulishTextTheme()*/
+    );
+
+final ThemeData darkTheme = ThemeData.dark(
+    /*brightness: Brightness.dark,
+  primarySwatch: Colors.indigo,
+    textTheme: GoogleFonts.mulishTextTheme()*/
+    );
