14e1763f4c99a8d49dc22fb42803619584c0606b
habyaad <adeomotoshan@gmail.com>
2024-01-02 22:18:06 +0100

    feat: implement home view

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:soro_soke/ui/common/general_button.dart';
 import 'package:soro_soke/utils/app_colors.dart';
 import 'package:stacked/stacked.dart';
+import '../../../models/chat_model.dart';
 import '../../../utils/ui_helpers.dart';
 import 'home_viewmodel.dart';
 
@@ -19,26 +20,86 @@ class HomeView extends StackedView<HomeViewModel> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppColors.backgroundColor,
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      'Hello, ${viewModel.currentUser!.displayName}!',
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.white
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        'Hello, ${viewModel.currentUser!.displayName}!',
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.white
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      verticalSpace(24),
+                      StreamBuilder(
+                        stream: viewModel.getFriends(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                            return const Center(
+                                child: Text(
+                                  "No chats, add friends to start chatting",
+                                  style: TextStyle(color: Colors.white),
+                                ));
+                          } else {
+                            List friends = snapshot.data!.docs.map((doc) {
+                              Map<String, dynamic> data = doc.data();
+                              print("data : $data");
+          
+                              return data;
+                            }).toList();
+          
+                            return ListView.separated(
+                              shrinkWrap: true,
+                              separatorBuilder: (ctx, idx) => verticalSpace(12),
+                              itemCount: friends.length,
+                              reverse: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              // Show the latest message at the bottom
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  minVerticalPadding: 12,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  tileColor: const Color(0xff493a5e),
+                                  onTap: () {
+                                    viewModel.goToChat(ChatModel(
+                                        name: friends[index]["name"],
+                                        uid: friends[index]["id"],
+                                        photoUrl: friends[index]["profilePhoto"]));
+                                  },
+                                  leading: CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage:
+                                    NetworkImage(friends[index]["profilePhoto"]),
+                                  ),
+                                  title: Text(
+                                    friends[index]["name"],
+                                    style: const TextStyle(
+                                        fontSize: 18, color: Colors.pinkAccent),
+                                  ),
+                                  subtitle: const Text("Tap to chat 💬",
+                                    style: TextStyle(
+                                        fontSize: 12, color: Colors.white70),
+                                  ),
+                                );
+                              },
+                            );
+                          }
+                        },
+                      ),
+          
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,16 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:soro_soke/app/app.locator.dart';
+import 'package:soro_soke/app/app.router.dart';
 import 'package:soro_soke/services/authentication_service.dart';
 import 'package:soro_soke/services/toast_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import '../../../models/chat_model.dart';
+import '../../../services/friend_service.dart';
 import '../../../services/user_service.dart';
 
 class HomeViewModel extends BaseViewModel {
   final _userService = locator<UserService>();
+  final _friendService = locator<FriendService>();
+  final _navigationService = locator<NavigationService>();
 
   User? currentUser;
 
   void initializeUser() async {
     currentUser = _userService.currentUser;
+  }
+
+  Stream<QuerySnapshot<Map<String, dynamic>>>? getFriends() {
+    return _friendService.getFriends();
+  }
+
+  void goToChat(ChatModel user) {
+    _navigationService.navigateToChatView(friend: user);
   }
 }
