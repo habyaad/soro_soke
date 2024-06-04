08df4394a98737e8f5bbcb13dd6a97a82208ece6
habyaad <adeomotoshan@gmail.com>
2024-06-04 17:03:05 +0100

    feat: fetch user profile

--- lib/ui/views/profile/profile_view.dart
+++ lib/ui/views/profile/profile_view.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
 import 'package:soro_soke/utils/app_colors.dart';
 import 'package:soro_soke/utils/ui_helpers.dart';
 import 'package:stacked/stacked.dart';
@@ -8,7 +12,7 @@ import '../../common/profile_detail_tile.dart';
 import 'profile_viewmodel.dart';
 
 class ProfileView extends StackedView<ProfileViewModel> {
-  const ProfileView({Key? key}) : super(key: key);
+  const ProfileView({super.key});
 
   @override
   Widget builder(
@@ -16,11 +20,24 @@ class ProfileView extends StackedView<ProfileViewModel> {
     ProfileViewModel viewModel,
     Widget? child,
   ) {
-    viewModel.initializeUser();
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: AppColors.backgroundColor,
-        body: Padding(
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+              onPressed: () {
+                viewModel.navigateToEditProfile();
+              },
+              icon: const Icon(
+                Icons.edit,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Center(
             child: Column(
@@ -28,45 +45,110 @@ class ProfileView extends StackedView<ProfileViewModel> {
                 const SizedBox(height: 20),
                 GestureDetector(
                   onTap: () {
-                    print('clicked');
+                    log('clicked');
                     viewModel.uploadPhoto();
                   },
-                  child: CircleAvatar(
-                    radius: 60,
-                    backgroundImage: NetworkImage(
-                        viewModel.currentUser!.photoURL.toString()),
+                  child: Stack(
+                    children: [
+                      CircleAvatar(
+                        radius: 60,
+                        backgroundImage: NetworkImage(
+                            viewModel.currentUser!.photoURL.toString()),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          width: 30,
+                          height: 30,
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.white),
+                          child: const Icon(
+                            Icons.camera_alt_rounded,
+                            color: AppColors.backgroundColor,
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
                 const SizedBox(height: 20),
                 Text(
                   viewModel.currentUser!.displayName.toString(),
                   style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white
-                  ),
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
                 ),
                 const SizedBox(height: 10),
                 Text(
                   viewModel.currentUser!.email.toString(),
-                  style: const TextStyle(fontSize: 16, color: Colors.grey,),
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
                 ),
                 const SizedBox(height: 20),
-                const ProfileDetailTile(
-                  icon: Icons.phone,
-                  label: 'Phone',
-                  value: '+234 123 456 7890',
+
+                FutureBuilder<DocumentSnapshot>(
+                  future: viewModel.profile,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return const Text("Something went wrong");
+                    }
+
+                    if (snapshot.hasData && !snapshot.data!.exists) {
+                      return const Text("Document does not exist");
+                    }
+
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      Map<String, dynamic> data =
+                          snapshot.data!.data() as Map<String, dynamic>;
+                      return Column(
+                        children: [
+                          ProfileDetailTile(
+                            icon: Icons.phone,
+                            label: 'Phone',
+                            value: data['phoneNumber']?? 'Nil',
+                          ),
+                          // const ProfileDetailTile(
+                          //   icon: Icons.location_on,
+                          //   label: 'Location',
+                          //   value: 'Lagos, Nigeria',
+                          // ),
+                          ProfileDetailTile(
+                            icon: Icons.work,
+                            label: 'About',
+                            value: data['about']?? 'Nil',
+                          ),
+                        ],
+                      );
+                    }
+
+                    return Shimmer(
+                      duration: const Duration(seconds: 4),
+                      //Default value
+                      interval: const Duration(seconds: 1),
+                      //Default value: Duration(seconds: 0)
+                      color: Colors.white,
+                      //Default value
+                      colorOpacity: 0.8,
+                      //Default value
+                      enabled: true,
+                      //Default value
+                      direction: const ShimmerDirection.fromLTRB(),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey.withOpacity(.1),
+                            borderRadius: BorderRadius.circular(8)),
+                        width: double.infinity,
+                        height: 100,
+                      ),
+                    );
+                  },
                 ),
-                const ProfileDetailTile(
-                  icon: Icons.location_on,
-                  label: 'Location',
-                  value: 'Lagos, Nigeria',
-                ),
-                const ProfileDetailTile(
-                  icon: Icons.work,
-                  label: 'Occupation',
-                  value: 'Software Engineer',
-                ),
+
                 // Add more profile detail tiles as needed
                 verticalSpaceMedium,
                 GeneralButton(
@@ -81,6 +163,12 @@ class ProfileView extends StackedView<ProfileViewModel> {
         ),
       ),
     );
+  }
+
+  @override
+  void onViewModelReady(ProfileViewModel viewModel) {
+    viewModel.initializeUser();
+    viewModel.profile = viewModel.fetchProfile();
   }
 
   @override
--- lib/ui/views/profile/profile_viewmodel.dart
+++ lib/ui/views/profile/profile_viewmodel.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:soro_soke/app/app.router.dart';
+import 'package:soro_soke/services/database_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 import '../../../app/app.locator.dart';
 import '../../../services/authentication_service.dart';
 import '../../../services/toast_service.dart';
@@ -7,11 +11,13 @@ import '../../../services/user_service.dart';
 
 class ProfileViewModel extends BaseViewModel {
   final _authService = locator<AuthenticationService>();
+  final _navigationService = locator<NavigationService>();
   final _toastService = locator<ToastService>();
   final _userService = locator<UserService>();
+  final _databaseService = locator<DatabaseService>();
 
   User? currentUser;
-
+  Future<DocumentSnapshot>? profile;
   void initializeUser() async {
     currentUser = _userService.currentUser;
   }
@@ -20,6 +26,14 @@ class ProfileViewModel extends BaseViewModel {
     await _userService.changeProfilePhoto();
     initializeUser();
     rebuildUi();
+  }
+
+  Future<DocumentSnapshot> fetchProfile() async {
+    return await _databaseService.fetchProfile(currentUser!.uid);
+  }
+
+  void navigateToEditProfile() async {
+    _navigationService.navigateToEditProfileView();
   }
 
   void logOut() async {
