3265a5292c700ddd4e1f279327655b1ad930201e
habyaad <adeomotoshan@gmail.com>
2024-06-11 10:20:23 +0100

    refactor

--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:soro_soke/services/user_service.dart';
 
@@ -9,24 +11,26 @@ class MessageService {
   final _databaseService = locator<DatabaseService>();
   final _userService = locator<UserService>();
 
-  String getConversationID(String friendUid) {
-    print("mine: ${_userService.currentUser!.uid.hashCode}");
-    print("yours: ${friendUid.hashCode}");
-    print("--------------------------------------");
+  String getConversationID(String friendUid, {senderId}) {
+    String sender = senderId ?? _userService.currentUser!.uid;
+    log("sender: ${sender.hashCode}");
+    log("receiver: ${friendUid.hashCode}");
+    log("--------------------------------------");
 
-    if (_userService.currentUser!.uid.hashCode < friendUid.hashCode) {
-      print("${_userService.currentUser!.uid}$friendUid");
-      return "${_userService.currentUser!.uid}$friendUid";
+    if (sender.hashCode < friendUid.hashCode) {
+      log("$sender$friendUid");
+      return "$sender$friendUid";
     } else {
-      print("$friendUid${_userService.currentUser!.uid}");
-      return "$friendUid${_userService.currentUser!.uid}";
+      log("$friendUid$sender");
+      return "$friendUid$sender";
     }
   }
 
 // Save a message
-  Future<void> saveMessage(Message message) async {
+  Future<void> saveMessage(Message message, {senderId}) async {
     await _databaseService.store
-        .collection('messages/${getConversationID(message.receiverId)}/chat')
+        .collection(
+            'messages/${getConversationID(message.receiverId, senderId: senderId)}/chat')
         .add({
       'senderId': message.senderId,
       'receiverId': message.receiverId,
@@ -38,16 +42,10 @@ class MessageService {
 // Retrieve messages between two users
 
   Stream<QuerySnapshot<Object?>> getMessagesBetweenUsers(String friendUid) {
-    try {
-      return _databaseService.store
-          .collection('messages/${getConversationID(friendUid)}/chat')
-          //.where("senderId", isEqualTo: user1Id)
-          .orderBy("timestamp", descending: true)
-          .snapshots();
-    } catch (e) {
-      print('Error retrieving messages: $e');
-      // Handle errors more gracefully in a production app
-      rethrow;
-    }
+    return _databaseService.store
+        .collection('messages/${getConversationID(friendUid)}/chat')
+        //.where("senderId", isEqualTo: user1Id)
+        .orderBy("timestamp", descending: true)
+        .snapshots();
   }
 }
